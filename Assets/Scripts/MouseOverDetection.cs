d2ac5b6636c28b56a0adecf4c01f162b5ab7aadc
GeorgeMichalakis <gmichalakis26@hotmail.com>
2023-03-19 17:44:45 +0100

    Mouseover/mouse click function/Main Game implementation

--- Assets/Scripts/MouseOverDetection.cs
+++ Assets/Scripts/MouseOverDetection.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseActionDetection : MonoBehaviour
+public class MouseOverDetection : MonoBehaviour
 {
+
     void OnMouseOver()
     {
         //If your mouse hovers over the GameObject with the script attached, output this message
         Debug.Log("Mouse is over GameObject.");
     }
-    void Update()
+
+    void OnMouseExit()
     {
-        if (Input.GetMouseButtonDown(0))
-            OnMouseOver();
+        //The mouse is no longer hovering over the GameObject so output this message each frame
+        Debug.Log("Mouse is no longer on GameObject.");
     }
 }
